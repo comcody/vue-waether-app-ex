b2d7a5a13fc405fcf0900cc55af859a90b3c3bd2
comcody <122582553+comcody@users.noreply.github.com>
2024-08-14 15:30:16 +0900

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="about-text">
+      <h2>About Weather App</h2>
+      <p>Developer: Test</p>
+    </div>
+  </div>
+</template>
+  
+<script setup>
+  
+</script>
+  
+<style>
+  
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="search-bar">
+    <form @submit.prevent>
+      <div class="form-group">
+        <input @input="inputText = $event.target.value" type="search" placeholder="지역을 입력해 주세요">
+        <!-- 1. 검색어 전달(commit으로 city state변경) -->
+        <!-- 2. 업데이트 된 city값으로 서버에 요청(dispatch) -->
+        <button @click="$store.commit('onSearchCity', inputText); $store.dispatch('getWeather')">
+          <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+        </button>
+      </div>
+    </form>
+  </div>  
+</template>
+  
+<script setup>
+  import { ref } from 'vue';
+
+  const inputText = ref('');
+  const emits = defineEmits(['onSearchCity']); // 이벤트를 정의하고 함수를 반환
+
+</script>
+  
+<style lang="scss" scoped>
+  .search-bar {
+    padding: 8px 20px;
+    border-radius: 40px;
+    background: #fff;
+    border: 1px solid #ccc;
+    padding: 0 20px;
+  
+    form {
+      .form-group {
+        display: flex;
+        input {
+          width: 100%;
+          border: none;
+          padding: 1em 0;
+          font-size: 18px;
+          outline: none;
+        }
+        button {
+          background: transparent;
+          border: none;
+          &:hover { cursor: pointer; }
+          .icon { font-size: 24px; }
+        }
+      }
+    }
+  }
+</style>
